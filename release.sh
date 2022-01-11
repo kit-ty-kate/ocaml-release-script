17e3e66d0a272b4a69ccba4b7e884157045f5a38
Kate <kit.ty.kate@disroot.org>
2022-01-11 17:35:04 +0000

    Add a new --force option

--- release.sh
+++ release.sh
@@ -2,12 +2,18 @@
 
 set -e
 
+case "${#}/${1}" in
+"0/") FORCE=false GIT_OPT="" ;;
+"1/--force") FORCE=true GIT_OPT="--force-with-lease" ;;
+*) echo "usage: ${0} [--force]"; exit 1 ;;
+esac
+
 ask() {
   echo "$1"
   read answer
   case "${answer}" in
   "Y"|"y"|"yes"|"Yes"|"") ;;
-  *) echo "Cancelling release..."; exit 1;;
+  *) echo "Cancelling release..."; exit 1 ;;
   esac
 }
 
@@ -30,15 +36,21 @@ git archive "${TAG}" --prefix "${NAME}-${VERSION}/" -o "${ARCHIVE}"
 
 ask "Is it ok to push the new tag and current branch to the remote \"origin\"? [Y/n] "
 
-git push origin "${TAG}"
-git push origin "${CURRENT_BRANCH}"
+git push ${GIT_OPT} origin "${TAG}"
+git push ${GIT_OPT} origin "${CURRENT_BRANCH}"
 cp "${ARCHIVE}" "_build/"
 dune-release publish doc
 
-echo "Now please create a new release at ${URL}/releases/new"
-echo "Here is the changelog to copy/past:"
-echo "${CHANGELOG}"
-echo
-echo "--------------------"
-echo "Please execute the follow command when you have created the release and uploaded the archive:"
-echo "opam publish \"${URL}/releases/download/${TAG}/${ARCHIVE}\""
+if ${FORCE}; then
+  echo "You can now update the release files at ${URL}/releases"
+  echo "Then call opam publish again:"
+  echo "opam publish \"${URL}/releases/download/${TAG}/${ARCHIVE}\""
+else
+  echo "Now please create a new release at ${URL}/releases/new"
+  echo "Here is the changelog to copy/past:"
+  echo "${CHANGELOG}"
+  echo
+  echo "--------------------"
+  echo "Please execute the follow command when you have created the release and uploaded the archive:"
+  echo "opam publish \"${URL}/releases/download/${TAG}/${ARCHIVE}\""
+fi
