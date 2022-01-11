161bde201a241967f141864003251d868b45e403
Kate <kit.ty.kate@disroot.org>
2022-01-11 15:54:32 +0000

    Push the tag and current branch to origin remote first

--- release.sh
+++ release.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+ask() {
+  echo "$1"
+  read answer
+  case "${answer}" in
+  "Y"|"y"|"yes"|"Yes"|"") ;;
+  *) echo "Cancelling release..."; exit 1;;
+  esac
+}
+
 NAME=$(grep "^(name " dune-project | sed -Ee "s/^\(name (.*)\)/\1/")
 VERSION=$(grep "^(version " dune-project | sed -Ee "s/^\(version (.*)\)/\1/")
 URL=$(grep "^dev-repo: " "${NAME}.opam" | sed -Ee "s/^dev-repo: \"git\+(.*)\.git\"/\1/")
@@ -9,20 +18,20 @@ URL=$(grep "^dev-repo: " "${NAME}.opam" | sed -Ee "s/^dev-repo: \"git\+(.*)\.git
 TAG=v${VERSION}
 ARCHIVE=${NAME}-${VERSION}.tbz
 CHANGELOG=$(git tag -n99 "${TAG}" | tail -n +3)
+CURRENT_BRANCH=$(git branch --show-current)
 
 dune-release tag
 dune-release check
 opam lint
 
-echo "Does that look alright? [Y/n] "
-read answer
-case "${answer}" in
-"Y"|"y"|"yes"|"Yes"|"") ;;
-*) echo "Cancelling release..."; exit 1;;
-esac
+ask "Does that look alright? [Y/n] "
 
 git archive "${TAG}" --prefix "${NAME}-${VERSION}/" -o "${ARCHIVE}"
 
+ask "Is it ok to push the new tag and current branch to the remote \"origin\"? [Y/n] "
+
+git push origin "${TAG}"
+git push origin "${CURRENT_BRANCH}"
 cp "${ARCHIVE}" "_build/"
 dune-release publish doc
 
